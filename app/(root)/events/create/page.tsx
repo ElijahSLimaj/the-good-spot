cf0f5a7973a430caa847a8b602befa62a9a4e870
Muhammad Salman Awan <m.sallman.awan@gmail.com>
2024-05-08 18:42:08 +0500

    use userId from publicMetaData

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,22 +1,26 @@
-import EventForm from "@/components/shared/EventForm"
-import { auth } from "@clerk/nextjs";
+import EventForm from "@/components/shared/EventForm";
+import { auth, currentUser } from "@clerk/nextjs";
 
-const CreateEvent = () => {
-  const { sessionClaims } = auth();
+const CreateEvent = async () => {
+  const user = await currentUser();
 
-  const userId = sessionClaims?.userId as string;
+  const userId = user?.publicMetadata.userId;
+
+  console.log("userId:", userId);
 
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Create Event
+        </h3>
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Create" />
+        <EventForm userId={userId as string} type="Create" />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CreateEvent
+export default CreateEvent;
